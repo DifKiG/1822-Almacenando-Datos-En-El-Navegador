f37145657b67ca9a8796e6ed8c728317d4cd8264
Diego Quijano <106574144+DifKiG@users.noreply.github.com>
2022-12-15 09:01:37 -0500

    faltaba el readTask en el script

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ import { readTasks } from "./components/readTasks.js";
 const btn = document.querySelector('[data-form-btn]');
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', addTask);
+readTasks();
